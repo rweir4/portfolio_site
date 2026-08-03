159cb9238a507c2cc9ecfcf58f9324c711d98806
rweir4 <rebeccaweir12@gmail.com>
2026-08-03 15:38:28 -0400

    Clean up/dry up code

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import { createRoot } from 'react-dom/client';
-import Portfolio from './App.jsx';
-import './index.css';
-
-const domNode = document.getElementById('root');
-const root = createRoot(domNode);
-root.render(<Portfolio />);
